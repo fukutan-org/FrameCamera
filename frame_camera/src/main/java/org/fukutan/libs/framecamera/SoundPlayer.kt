6be5129796ba045fc53731bb7c37ebe1ee56765e
fukutan-org <fukutan.github@gmail.com>
2019-06-27 18:03:37 +0900

    improve change way to play sound

--- frame_camera/src/main/java/org/fukutan/libs/framecamera/SoundPlayer.kt
+++ frame_camera/src/main/java/org/fukutan/libs/framecamera/SoundPlayer.kt
@@ -1,26 +1,54 @@
 package org.fukutan.libs.framecamera
 
 import android.content.res.AssetFileDescriptor
+import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.media.SoundPool
+import android.renderscript.RenderScript
 import android.util.Log
 
 class SoundPlayer(descriptor: AssetFileDescriptor) {
 
-    private val player: MediaPlayer
+//    private val player: MediaPlayer
+    private val player: SoundPool
+    private val soundId: Int
+    private var loaded = false
 
     init {
-        val mp = MediaPlayer()
-        mp.setDataSource(descriptor.fileDescriptor, descriptor.startOffset, descriptor.length)
-        mp.setOnPreparedListener {
-            Log.d("MusicPlayer", "OnPrepared")
-        }
-        mp.prepareAsync()
-        mp.isLooping = false
 
-        player = mp
+        val attr = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+        player = SoundPool.Builder()
+            .setAudioAttributes(attr)
+            .setMaxStreams(1)
+            .build()
+        soundId = player.load(descriptor, 1)
+
+        player.setOnLoadCompleteListener { soundPool, sampleId, status ->
+
+            if (status == 0) {
+                loaded = true
+            }
+        }
+
+//        val mp = MediaPlayer()
+//        mp.setDataSource(descriptor.fileDescriptor, descriptor.startOffset, descriptor.length)
+//        mp.setOnPreparedListener {
+//            Log.d("MusicPlayer", "OnPrepared")
+//        }
+//        mp.prepareAsync()
+//        mp.isLooping = false
+//
+//        player = mp
     }
 
     fun play() {
-        player.start()
+        if (loaded) {
+            player.play(soundId, 1.0f, 1.0f, 0, 0, 1f)
+        } else {
+            // TODO
+        }
     }
 }
